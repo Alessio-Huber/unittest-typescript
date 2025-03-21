471b74508d3e5c5a3c0a7803783edb6988d3b892
Alessio Huber <alessio_huber@sluz.ch>
2025-03-21 15:28:47 +0100

    Aufgabe 5.1, 5.2 und 5.3 Tests geschrieben und sie funktionieren

--- src/average.test.ts
+++ src/average.test.ts
@@ -1,13 +1,28 @@
 import { mean } from "./average";
 
-test("test mean of two numbers", () => {
-  // Arrange
-  const numbers: number[] = [1, 4];
-  const expected: number = 2.5;
+test("mean of two numbers", () => {
+  
+  const numbers: number[] = [4, 8];  
+  const expected: number = 6; 
 
-  // Act
-  const actual: number = mean(numbers);
+  const actual = mean(numbers);
 
-  // Assert
   expect(actual).toBe(expected);
+});
+
+test("mean of five numbers", () => {
+
+  const numbers: number[] = [2, 4, 6, 8, 10];
+  const expected: number = 6;
+
+  const actual = mean(numbers);
+
+  expect(actual).toBe(expected);
+});
+
+test("mean of no numbers throws error", () => {
+ 
+  const numbers: number[] = [];
+
+  expect(() => mean(numbers)).toThrow("Liste darf nicht leer sein");
 });
